e18a0af258f12d017f37d51d7affc91d6d12b235
piumin <67179701+piumin@users.noreply.github.com>
2020-08-22 22:04:41 +0530

    Add files via upload
    sketching of client site and the api

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import data from './data';
+import './App.css';
+
+function App() {
+
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.add("open");
+  }
+
+  const closeMenu = () => {
+    document.querySelector(".sidebar").classList.remove("open");
+  }
+
+  return (
+    <div className="grid-container">
+    <header className="header">
+      <div className="brand">
+        <button onClick={openMenu}>
+          &#9776;
+        </button>
+        <a href="index.html">Neuro Cart</a>
+      </div>
+      <div className="header-links">
+        <a href="cart.html">Cart</a>
+        <a href="signin.html">Sign In</a>
+      </div>
+    </header>
+    <aside className="sidebar">
+      <h3>Shopping Categories</h3>
+      <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+      <ul>
+        <li>
+          <a href="index.html">Laptops</a>
+        </li>
+
+        <li>
+          <a href="index.html">Desktop</a>
+        </li>
+
+        <li>
+          <a href="index.html">Tablet</a>
+          </li>
+          
+          <li>
+          <a href="index.html">Storage and Hardware devices</a>
+        </li>
+
+      </ul>
+    </aside>
+    <main className="main">
+      <div className="content">
+          <ul className="products">
+            {
+              data.products.map(product =>
+                <li>
+                  <div className="product">
+                    <img className="product-image" src={product.image} alt="product" />
+                    <div className="product-name">
+                      <a href="product.html">{product.name}</a>
+                    </div>
+                    <div className="product-brand">{product.brand}</div>
+                    <div className="product-price">{product.price}</div>
+                    <div className="product-rating">{product.rating}</div>
+                  </div>
+                </li>)
+            
+            }
+      </ul>
+      </div>
+
+    </main>
+    <footer className="footer">
+      All right reserved.
+    </footer>
+  </div>
+  );
+}
+
+export default App;
