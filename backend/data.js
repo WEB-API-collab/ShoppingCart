4ecc483d8d181c7ea614f07cf004f4ad5317eeaf
RavinduVidarshana <39744902+RavinduVidarshana@users.noreply.github.com>
2020-08-27 02:37:45 +0530

    Add files via upload
    backend

---
+++ backend/data.js
@@ -0,0 +1,48 @@
+export default   {
+    products: [
+    {
+        _id:'1',
+        name:  'Gaming Laptop',
+        category: 'Laptop',
+        image: '/images/GamingMSI1.jpg',
+        price: 60,
+        brand: 'MSI',
+        rating: 4.5,
+        numReviews:10,
+        countInStock: 6
+    },
+    {
+        _id:'2',
+        name:  'Working Laptop',
+        category: 'Laptop',
+        image: '/images/GamingMSI1.jpg',
+        price: 90,
+        brand: 'MSI',
+        rating: 5.5,
+        numReviews:10
+    },
+    {
+        _id:'3',
+        name:  'PC',
+        category: 'Dextop',
+        image: '/images/GamingMSI1.jpg',
+        price: 120,
+        brand: 'MSI',
+        rating: 6.5,
+        numReviews:10,
+        countInStock: 4
+    },
+    {
+        _id:'4',
+        name:  'PC',
+        category: 'Dextop',
+        image: '/images/GamingMSI1.jpg',
+        price: 180,
+        brand: 'MSI',
+        rating: 9.5,
+        numReviews:20,
+        countInStock: 0
+    },
+
+    ]
+}
