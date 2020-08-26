20185ab911b05ed7ec9cfd4e1bbba349c81cecee
RavinduVidarshana <39744902+RavinduVidarshana@users.noreply.github.com>
2020-08-27 02:56:14 +0530

    Qty add to cart

---
+++ frontend/src/data.js
@@ -0,0 +1,35 @@
+export default   {
+    products: [
+    {
+        _id:'1',
+        name:  'Gaming Laptop',
+        category: 'Laptop',
+        image: '/images/GamingMSI1.jpg',
+        price: 60,
+        brand: 'MSI',
+        rating: 4.5,
+        numReviews:10
+    },
+    {
+        _id:'2',
+        name:  'Working Laptop',
+        category: 'Laptop',
+        image: '/images/GamingMSI1.jpg',
+        price: 90,
+        brand: 'MSI',
+        rating: 5.5,
+        numReviews:10
+    },
+    {
+        _id:'3',
+        name:  'PC',
+        category: 'Dextop',
+        image: '/images/GamingMSI1.jpg',
+        price: 120,
+        brand: 'MSI',
+        rating: 6.5,
+        numReviews:10
+    },
+
+    ]
+}
