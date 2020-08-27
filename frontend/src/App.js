b6115608ec6695f74a797ce20815b3b06d4eae26
Ruvinda Lahiru Samaranayake <androidruvee@gmail.com>
2020-08-27 22:21:29 +0530

    Register,SignIn & Manage Products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,15 @@ import './App.css';
 import HomeScreen from './sscreens/HomeScreen';
 import ProductScreen from './sscreens/ProductScreen';
 import CartScreen from './sscreens/CartScreen';
+import SigninScreen from './sscreens/SignInScreen';
+import { useSelector } from 'react-redux';
+import RegisterScreen from './sscreens/RegisterScreen';
+import ProductsScreen from './sscreens/ProductsScreen';
 
 function App() {
+
+  const userSignin = useSelector(state => state.userSignin);
+  const { userInfo } = userSignin;
 
   const openMenu = () => {
     document.querySelector(".sidebar").classList.add("open");     
@@ -31,7 +38,10 @@ function App() {
       <div className="header-links">
         <a href="index.html">Home</a>
         <a href="cart.html">My Cart</a>
-        <a href="signin.html">Sign In</a>
+            {
+              userInfo ? <Link to="/profile">{userInfo.name}</Link> :
+                <Link to="/signin">Sign In</Link>
+            }
       </div>
     </header> 
 
@@ -55,7 +65,10 @@ function App() {
 
     <main className="main">
         <div className="content">
-            
+
+            <Route path="/products" component={ProductsScreen} />
+            <Route path="/signin" component={SigninScreen} />
+            <Route path="/register" component={RegisterScreen} />            
             <Route path="/product/:id" component={ProductScreen} />
             <Route path="/cart/:id?" component={CartScreen} />
             <Route path="/" exact={true} component={HomeScreen} />
