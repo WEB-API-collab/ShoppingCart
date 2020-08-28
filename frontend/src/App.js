6e34024296bd487fc3bceadb59c0d62624bd169a
RavinduVidarshana <39744902+RavinduVidarshana@users.noreply.github.com>
2020-08-28 14:58:05 +0530

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,59 +1,69 @@
 import React from 'react';
-//import data from './data';
-import {BrowserRouter, Route,Link} from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
+import SigninScreen from './screens/SigninScreen';
+import { useSelector } from 'react-redux';
+import RegisterScreen from './screens/RegisterScreen';
+import ProductsScreen from './screens/ProductsScreen';
 
 function App() {
 
-  const openMenu = () =>{
+  const userSignin = useSelector(state => state.userSignin);
+  const { userInfo } = userSignin;
+  const openMenu = () => {
     document.querySelector(".sidebar").classList.add("open");
   }
-
   const closeMenu = () => {
-    document.querySelector(".sidebar").classList.remove("open");
+    document.querySelector(".sidebar").classList.remove("open")
   }
   return (
-                                
-    <BrowserRouter>                                                
-    <div className="grid-container">
-    <header className="header">
-        <div className="brand">
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="header">
+          <div className="brand">
             <button onClick={openMenu}>
-                &#9776;
-            </button>
-            <Link to = "/" >NeuroCart</Link>
-            
-        </div>
-        <div className="header-links">
+              &#9776;
+        </button>
+            <Link to="/" >neurocart</Link>
+          </div>
+          <div className="header-links">
             <a href="cart.html">Cart</a>
-            <a href="signin.html">Sign In</a>
-        </div>    
-    </header>
-    <aside className="sidebar">
-        <h3>Shopping Categories</h3>
-        <button className="sidebar-close-button" onClick={closeMenu}>X</button>
-        <ul>
-            <li><a href="index.html">Laptop</a></li>
-            <li><a href="index.html">Dextop</a></li>
-        </ul>
-    </aside>
-  
-    <main className="main">
-        <div className="content">
-            <Route path="/product/:id" component = {ProductScreen} />
-            <Route path="/" exact ={true} component = {HomeScreen} />
-           
-        </div>
-       
-    </main>
-    <footer className="footer">
-        All right reserved.
+            {
+              userInfo ? <Link to="/profile">{userInfo.name}</Link> :
+                <Link to="/signin">Sign In</Link>
+            }
+          </div>
+        </header>
+        <aside className="sidebar">
+          <h3>Shopping Categories</h3>
+          <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+          <ul>
+            <li>
+              <a href="index.html">Laptop</a>
+            </li>
+            <li>
+              <a href="index.html">Dextop</a>
+            </li>
+          </ul>
+        </aside>
+        <main className="main">
+          <div className="content">
+            <Route path="/products" component={ProductsScreen} />
+            <Route path="/signin" component={SigninScreen} />
+            <Route path="/register" component={RegisterScreen} />
+            <Route path="/product/:id" component={ProductScreen} />
+            <Route path="/cart/:id?" component={CartScreen} />
+            <Route path="/" exact={true} component={HomeScreen} />
+          </div>
+        </main>
+        <footer className="footer">
+          All right reserved.
     </footer>
-    </div>
+      </div>
     </BrowserRouter>
-     );
+  );
 }
-
 export default App;
