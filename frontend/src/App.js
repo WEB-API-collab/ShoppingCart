2b564e3c739e8203e75def9d83b77899975d7863
Ruvinda Lahiru Samaranayake <androidruvee@gmail.com>
2020-09-02 15:52:14 +0530

    Created NeuroBot Chat using Dialogflow (api.ai)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import ShippingScreen from './sscreens/ShippingScreen';
 import PaymentScreen from './sscreens/PaymentScreen';
 import PlaceOrderScreen from './sscreens/PlaceOrderScreen';
 import OrderScreen from './sscreens/OrderScreen';
+import KommunicateChat from './chat';
 
 
 function App() {
@@ -49,6 +50,7 @@ function App() {
                 <Link to="/signin">Sign In</Link>
             }
       </div>
+      
     </header> 
 
     <aside className ="sidebar">
@@ -78,6 +80,7 @@ function App() {
             <Route path="/product/:id" component={ProductScreen} />
             <Route path="/cart/:id?" component={CartScreen} />
             <Route path="/" exact={true} component={HomeScreen} />
+            <Route path="/" exact={true} component={KommunicateChat} />
             
           </div>
     </main>
